2989f94aea4fe8d20d090e3c9be89425c87a002e
Santiago Jara <swja1991@hotmail.com>
2018-01-22 14:53:17 -0500

    Agregando controladores aun funciona, estructura realizada, decidir si hacer con server o mysql

---
+++ Helix2.0/Controller/forma_pagoController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Helix2._0.Model;
+
+namespace Helix2._0.Controller
+{
+    public class forma_pagoController : ApiController
+    {
+        private HelixEntities db = new HelixEntities();
+
+        // GET: api/forma_pago
+        public IQueryable<forma_pago> Getforma_pago()
+        {
+            return db.forma_pago;
+        }
+
+        // GET: api/forma_pago/5
+        [ResponseType(typeof(forma_pago))]
+        public IHttpActionResult Getforma_pago(int id)
+        {
+            forma_pago forma_pago = db.forma_pago.Find(id);
+            if (forma_pago == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(forma_pago);
+        }
+
+        // PUT: api/forma_pago/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putforma_pago(int id, forma_pago forma_pago)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != forma_pago.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(forma_pago).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!forma_pagoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/forma_pago
+        [ResponseType(typeof(forma_pago))]
+        public IHttpActionResult Postforma_pago(forma_pago forma_pago)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.forma_pago.Add(forma_pago);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (forma_pagoExists(forma_pago.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = forma_pago.ID }, forma_pago);
+        }
+
+        // DELETE: api/forma_pago/5
+        [ResponseType(typeof(forma_pago))]
+        public IHttpActionResult Deleteforma_pago(int id)
+        {
+            forma_pago forma_pago = db.forma_pago.Find(id);
+            if (forma_pago == null)
+            {
+                return NotFound();
+            }
+
+            db.forma_pago.Remove(forma_pago);
+            db.SaveChanges();
+
+            return Ok(forma_pago);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool forma_pagoExists(int id)
+        {
+            return db.forma_pago.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ Helix2.0/Controller/rolController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Helix2._0.Model;
+
+namespace Helix2._0.Controller
+{
+    public class rolController : ApiController
+    {
+        private HelixEntities db = new HelixEntities();
+
+        // GET: api/rol
+        public IQueryable<rol> Getrol()
+        {
+            return db.rol;
+        }
+
+        // GET: api/rol/5
+        [ResponseType(typeof(rol))]
+        public IHttpActionResult Getrol(int id)
+        {
+            rol rol = db.rol.Find(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rol);
+        }
+
+        // PUT: api/rol/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putrol(int id, rol rol)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != rol.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(rol).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!rolExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/rol
+        [ResponseType(typeof(rol))]
+        public IHttpActionResult Postrol(rol rol)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.rol.Add(rol);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (rolExists(rol.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = rol.ID }, rol);
+        }
+
+        // DELETE: api/rol/5
+        [ResponseType(typeof(rol))]
+        public IHttpActionResult Deleterol(int id)
+        {
+            rol rol = db.rol.Find(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            db.rol.Remove(rol);
+            db.SaveChanges();
+
+            return Ok(rol);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool rolExists(int id)
+        {
+            return db.rol.Count(e => e.ID == id) > 0;
+        }
+    }
+}
